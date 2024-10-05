40539ae109150600dbbdf9c954f740e0166dc882
shravanighule <shravanighule16@gmail.com>
2024-10-06 00:44:43 +0530

    Add files via upload

---
+++ method inside class.cpp
@@ -0,0 +1,19 @@
+//Defining method in class.
+#include<iostream>
+using namespace std;
+
+class student               
+{
+    public:
+    void myMethod()
+    {
+        cout<<"I am studying in 2nd year.";
+    }
+};
+int main()
+{
+    student s1;
+    s1.myMethod();
+}
+
+//I am studying in 2nd year.
